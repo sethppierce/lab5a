30430f981b9ce16bf4f9eb585465a5203e980d57
Seth <sethparkerpierce@icloud.com>
2022-09-16 16:46:12 -0400

    wasn't working in branch

--- js/app.js
+++ js/app.js
@@ -104,11 +104,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+ 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
